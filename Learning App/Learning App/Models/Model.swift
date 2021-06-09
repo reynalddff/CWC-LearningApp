0fe1d8ec16b6c3579a3c3c4ce62054507c412647
Reynald Pahlevi <35914709+reynalddff@users.noreply.github.com>
2021-06-09 21:54:19 +0700

    Parsing JSON - add assets - add models - add data files - parse json in content model - parse style data in content model

---
+++ Learning App/Learning App/Models/Model.swift
@@ -0,0 +1,45 @@
+
+//  Model.swift
+//  Learning App
+//
+//  Created by Reynald Daffa Pahlevi on 09/06/21.
+
+
+import Foundation
+struct Module: Decodable, Identifiable {
+    var id: Int
+    var category: String
+    var content: Content
+    var test: Test
+}
+
+struct Content: Decodable, Identifiable {
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lessons: [Lesson]
+}
+
+struct Lesson: Decodable, Identifiable {
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explanation: String
+}
+
+struct Test: Decodable, Identifiable {
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var questions: [Question]
+}
+
+struct Question: Decodable, Identifiable {
+    var id: Int
+    var content: String
+    var correctIndex: String
+    var answers: [String]
+}
--- Learning App/Learning App/ViewModels/ContentModel.swift
+++ Learning App/Learning App/ViewModels/ContentModel.swift
@@ -5,8 +5,87 @@
 //  Created by Reynald Daffa Pahlevi on 09/06/21.
 //
 
+//import Foundation
+//
+//class ContentModel: ObservableObject{
+//    @Published var modules = [Module]()
+//
+//    init () {
+//        getLocalData()
+//    }
+//
+//
+//    func getLocalData() {
+//
+//        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+//
+//        do {
+//            let jsonData = try Data(contentsOf: jsonUrl!)
+//
+//            let jsonDecoder = JSONDecoder()
+//            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+//
+//            self.modules = modules
+//
+//        } catch {
+//            print("Couldn't parse local data")
+//        }
+//
+//
+//
+//    }
+//}
+
 import Foundation
 
-class ContentModel: ObservableObject{
+class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        
+        getLocalData()
+        
+    }
+    
+    func getLocalData() {
+        
+        // Get a url to the json file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            // Read the file into a data object
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            // Try to decode the json into an array of modules
+            let jsonDecoder = JSONDecoder()
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            // Assign parsed modules to modules property
+            self.modules = modules
+        }
+        catch {
+            // TODO log error
+            print("Couldn't parse local data")
+        }
+        
+        // Parse the style data
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            
+            // Read the file into a data object
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+        }
+        catch {
+            // Log error
+            print("Couldn't parse style data")
+        }
+        
+    }
 }
+
